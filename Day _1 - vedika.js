08aa15e1451e2c37bfbeb406f5e07f7cff7a0774
Vedika Gupta <145323225+vedikagupta08@users.noreply.github.com>
2025-08-26 21:35:51 +0530

    Day _1 - vedika.js

---
+++ Day _1 - vedika.js
@@ -0,0 +1,18 @@
+// student.js
+
+// Create a student object
+const student = {
+    name: "Vedika Gupta",
+    age: 20,
+    branch: "Computer Science and Engineering",
+    rollNo: "1234",
+    college: "Amity UNiversity Madhya Pradesh"
+};
+
+// Print student details
+console.log("----- Student Details -----");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Branch: " + student.branch);
+console.log("Roll No: " + student.rollNo);
+console.log("College: " + student.college);
